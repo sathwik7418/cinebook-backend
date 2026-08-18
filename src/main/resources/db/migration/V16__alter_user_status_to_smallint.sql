ef2041240626dc54b2d739526dafe34fc1e5e18a
Sathwik7418 <sathwikvangari3@gmail.com>
2026-08-18 12:17:11 +0530

    Fix duplicate Flyway V16 migration

--- src/main/resources/db/migration/V16__alter_user_status_to_smallint.sql
+++
@@ -1,3 +0,0 @@
--- V16: Change users.status column type from INTEGER to SMALLINT to match Hibernate enum mapping
--- User.entity has status field (enum) without @Column, defaults to ordinal stored as smallint
-ALTER TABLE users ALTER COLUMN status TYPE SMALLINT USING status::SMALLINT;
